ea52743b54c3f5922d79c24e9840ebbb7e95be91
abircse45 <abiruodacse@gmail.com>
2022-10-22 18:27:52 +0600

    added

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,58 +1,89 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-import '../model/note_model.dart';
+import '../model/todo_model.dart';
 
-class DataBaseHelper {
+class DbHelper {
   static Database? _database;
 
   Future<Database?> get db async {
     if (_database != null) {
-      return _database;
+      return _database!;
     }
 
-    _database = await initialDatabase();
+    _database = await initDatabase();
     return _database;
   }
 
-  initialDatabase() async {
-    Directory dpath = await getApplicationDocumentsDirectory();
+  String TABLE_NAME = "todos.db";
 
-    String path = join(dpath.path, "note.db");
-   return openDatabase(path, version: 1, onCreate: _onCreate);
+  Future<Database> initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, TABLE_NAME);
+    return openDatabase(path, version: 1, onCreate: _onCreate);
   }
 
   _onCreate(Database db, int version) async {
-    db.execute('''
-      CREATE TABLE note(
-      id INTEGER PRIMARY KEY,
-      studentid TEXT,
-      name TEXT,
-      email TEXT,
-      department TEXT,
-      university TEXT,
-      datetime TEXT
-      
-      )
-      ''');
+    await db.execute('''
+    CREATE TABLE todos 
+    (  
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    dateTime TEXT
+    
+    )
+   
+    ''');
   }
 
-  Future addNote(NoteModel noteModel) async {
+  Future addTodo(TodoModel todoModel) async {
     Database? database = await db;
-
-    return await database!.insert("note", noteModel.toJson());
+    return await database!.insert('todos', todoModel.toJson());
   }
 
-  Future<List<NoteModel>?> getNoteModel() async {
+  Future<List<TodoModel>?> getTodoModel() async {
     Database? database = await db;
+    var data = await database!.query("todos", orderBy: "id");
 
-    var data = await database!.query("note", orderBy: "id");
+    List<TodoModel> todoModels =
+        data.map((todoModel) => TodoModel.fromJson(todoModel)).toList();
 
-    List<NoteModel> noteModels =
-        data.map((noteModel) => NoteModel.fromJson(noteModel)).toList();
-
-    return noteModels;
+    return todoModels;
   }
+  
+  
+  updateTodo(TodoModel todoModel)async{
+    Database? database = await db;
+    return database!.update("todos",todoModel.toJson(),where: 'id = ?',whereArgs: [todoModel.id]);
+
+
+  }
+
+
+  delete(int id)async{
+    Database? database = await db;
+   return database!.delete("todos",where: 'id = ?',whereArgs: [id] );
+
+
+
+
+
+
+
+  }
+
+  // deleteTodo(int id) async {
+  //   Database? database = await db;
+  //   database!.delete("todos", where: 'id = ?', whereArgs: [id]);
+  // }
+  //
+  // updateTodo(TodoModel todoModel) async {
+  //   Database? database = await db;
+  //   database!.update("todos", todoModel.toJson(),
+  //       where: 'id = ?', whereArgs: [todoModel.id]);
+  // }
 }
